e48d58fa86144323a5fc556b6a4af86b75e10b46
Yinyi Chen <yinyic@amazon.com>
2022-03-17 17:12:35 -0700

    fmt

--- scripts/install-ecs-init.sh
+++ scripts/install-ecs-init.sh
@@ -10,19 +10,19 @@ WORK_DIR="$(mktemp -d)"
 trap "rm -rf ${WORK_DIR}" EXIT
 
 if [ -z "$INIT_LOCAL_OVERRIDE" ]; then # no local overrides, retrieve from S3
-	if [ -z "$ECS_INIT_URL" ]; then # S3 URL not specified, use standard path
-	    ARCH=$(uname -m)
-	    host_suffix=""
-	    if grep -q "^cn-" <<<"$REGION"; then
-	        host_suffix=".cn"
-	    fi
-	    ECS_INIT_URL="https://s3.$REGION.amazonaws.com${host_suffix}/amazon-ecs-agent-$REGION/ecs-init-$AGENT_VERSION-$INIT_REV.$AL_NAME.$ARCH.rpm"
-	fi
-	curl -fLSs -o "$WORK_DIR/ecs-init.rpm" "$ECS_INIT_URL"
-	curl -fLSs -o "$WORK_DIR/ecs-init.rpm.asc" "${ECS_INIT_URL}.asc"
+    if [ -z "$ECS_INIT_URL" ]; then    # S3 URL not specified, use standard path
+        ARCH=$(uname -m)
+        host_suffix=""
+        if grep -q "^cn-" <<<"$REGION"; then
+            host_suffix=".cn"
+        fi
+        ECS_INIT_URL="https://s3.$REGION.amazonaws.com${host_suffix}/amazon-ecs-agent-$REGION/ecs-init-$AGENT_VERSION-$INIT_REV.$AL_NAME.$ARCH.rpm"
+    fi
+    curl -fLSs -o "$WORK_DIR/ecs-init.rpm" "$ECS_INIT_URL"
+    curl -fLSs -o "$WORK_DIR/ecs-init.rpm.asc" "${ECS_INIT_URL}.asc"
 else
-	mv "/tmp/additional-packages/$ECS_INIT_LOCAL_OVERRIDE" "$WORK_DIR/ecs-init.rpm"
-	mv "/tmp/additional-packages/$ECS_INIT_LOCAL_OVERRIDE.asc" "$WORK_DIR/ecs-init.rpm.asc"
+    mv "/tmp/additional-packages/$ECS_INIT_LOCAL_OVERRIDE" "$WORK_DIR/ecs-init.rpm"
+    mv "/tmp/additional-packages/$ECS_INIT_LOCAL_OVERRIDE.asc" "$WORK_DIR/ecs-init.rpm.asc"
 fi
 
 gpg --import "/tmp/$PGP_KEY_FILE"
