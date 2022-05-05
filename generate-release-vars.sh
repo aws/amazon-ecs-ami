fa82212c4b83352ffe9264ea499df12a93d17f6a
Anuj Singh <singholt@amazon.com>
2022-05-05 15:31:35 -0700

    ami_name_x86 -> ami_name_al2_x86 in generate-release-vars.sh

--- generate-release-vars.sh
+++ generate-release-vars.sh
@@ -61,7 +61,7 @@ ami_name_al2022_arm=$(aws ec2 describe-images --region "$region" --owner amazon 
 # Get the latest AL2022 distribution release
 distribution_release_al2022=$(curl "https://al2022-repos-us-west-2-9761ab97.s3.dualstack.us-west-2.amazonaws.com/core/releasemd.xml" | grep -o 'release version="[^"]*"' | cut -f2 -d '"' | sort -r | head -1)
 
-readonly ami_name_al2_arm ami_name_x86 ami_name_al1 ami_name_al2022_arm ami_name_al2022_x86 distribution_release_al2022
+readonly ami_name_al2_arm ami_name_al2_x86 ami_name_al1 ami_name_al2022_arm ami_name_al2022_x86 distribution_release_al2022
 
 cat >|release.auto.pkrvars.hcl <<EOF
 ami_version          = "$ami_version"
