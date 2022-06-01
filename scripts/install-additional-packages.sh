e36ad3a6fa51e850946ef7b6777680c36850fe92
Heming Han <hanhm@amazon.com>
2022-06-01 14:05:55 -0700

    Change find to ls

--- scripts/install-additional-packages.sh
+++ scripts/install-additional-packages.sh
@@ -4,11 +4,15 @@ set -ex
 ARCH=$(uname -m)
 
 # install any rpm packages from the additional-packages/ directory
-if [[ $(sudo find /tmp/additional-packages/*."${ARCH}".rpm -type f | sudo wc -l) -gt 0 ]]; then
+if ls /tmp/additional-packages/*."${ARCH}".rpm; then
     echo "Found additional packages with architecture ${ARCH} to be installed"
     sudo yum localinstall -y /tmp/additional-packages/*."${ARCH}".rpm
+else
+    echo "No matching additional packages with architecture ${ARCH} found"
 fi
-if [[ $(sudo find /tmp/additional-packages/*.noarch.rpm -type f | sudo wc -l) -gt 0 ]]; then
+if ls /tmp/additional-packages/*.noarch.rpm; then
     echo "Found additional packages with no specific architecture to be installed"
     sudo yum localinstall -y /tmp/additional-packages/*.noarch.rpm
+else
+    echo "No matching additional packages with no architecture found"
 fi
