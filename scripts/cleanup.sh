3604141e8696a1abfbe2d76784af5af4c58b657c
Cameron Sparr <sparrc@users.noreply.github.com>
2022-07-07 16:28:31 -0700

    add network-online.target to containerd service dependencies

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,6 +1,37 @@
 #!/usr/bin/env bash
 set -ex
 
+cat <<EOF > /tmp/containerd.service
+[Unit]
+Description=containerd container runtime
+Documentation=https://containerd.io
+After=network-online.target local-fs.target
+Wants=network-online.target
+
+[Service]
+ExecStartPre=-/sbin/modprobe overlay
+ExecStart=/usr/bin/containerd
+
+Type=notify
+Delegate=yes
+KillMode=process
+Restart=always
+RestartSec=5
+# Having non-zero Limit*s causes performance problems due to accounting overhead
+# in the kernel. We recommend using cgroups to do container-local accounting.
+LimitNPROC=infinity
+LimitCORE=infinity
+LimitNOFILE=1048576
+# Comment TasksMax if your systemd version does not supports it.
+# Only systemd 226 and above support this version.
+TasksMax=infinity
+OOMScoreAdjust=-999
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo mv /tmp/containerd.service /usr/lib/systemd/system/containerd.service
+
 sudo yum clean all
 
 function cleanup() {
