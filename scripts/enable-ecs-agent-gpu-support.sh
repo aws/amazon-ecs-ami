fbb759caed3e11fe9a5daa402b67795edd971532
Saisree Valluri <vsaisree@amazon.com>
2023-09-26 18:03:05 +0000

    Enable Persistence mode in userdataALTemplate

--- scripts/enable-ecs-agent-gpu-support.sh
+++ scripts/enable-ecs-agent-gpu-support.sh
@@ -43,9 +43,12 @@ sudo yum install -y kernel-devel-$(uname -r) \
 sudo yum install -y cuda-drivers \
     cuda
 
-# The Fabric Manager service needs be started and enabled on EC2 P4d instances
+# The Fabric Manager service needs to be started and enabled on EC2 P4d instances
 # in order to configure NVLinks and NVSwitches
 sudo systemctl enable nvidia-fabricmanager
+# NVIDIA Persistence Daemon needs to be started and enabled on P5 instances
+# to maintain persistent software state in the NVIDIA driver.
+sudo systemctl enable nvidia-persistenced
 mkdir -p /tmp/ecs
 echo 'ECS_ENABLE_GPU_SUPPORT=true' >>/tmp/ecs/ecs.config
 sudo mv /tmp/ecs/ecs.config /var/lib/ecs/ecs.config
