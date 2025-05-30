cd794d2b6515bac1e1410f297f076f8a39286e58
Thean Lim <theanlim@amazon.com>
2025-05-30 11:49:34 -0700

    Unpin kernel version

--- scripts/al2/install-kernel5dot10.sh
+++ scripts/al2/install-kernel5dot10.sh
@@ -5,7 +5,6 @@
 set -ex
 
 if [[ $AMI_TYPE == "al2kernel5dot10"* ]]; then
-    sudo amazon-linux-extras enable kernel-5.10
-    sudo yum install -y kernel-5.10.235
+    sudo amazon-linux-extras install -y kernel-5.10
     sudo rpm -e kernel-4.*
 fi
