0f7ac7168c5de4610d04950610476e29dc819f2c
Dane H Lim <slimdane@amazon.com>
2025-09-02 16:38:14 -0700

    Temporarily pin ecs-service-connect-agent package to v1.29.12.1

--- scripts/install-service-connect-appnet.sh
+++ scripts/install-service-connect-appnet.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -ex
 
-sudo yum install -y ecs-service-connect-agent
+# Temporarily pin to version 1.29.12.1 as latest available version 1.29.12.2 in Amazon Linux repos has a known issue.
+sudo yum install -y ecs-service-connect-agent-v1.29.12.1-*
