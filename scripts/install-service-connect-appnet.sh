505dc3d02cba46f6f41bd01596e8e1410b4bcc83
Cameron Sparr <sparrc@users.noreply.github.com>
2026-02-20 09:22:45 -0800

    Temporarily pin ecs-service-connect-agent package to v1.34.4.2 (#629)

--- scripts/install-service-connect-appnet.sh
+++ scripts/install-service-connect-appnet.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -ex
 
-sudo yum install -y ecs-service-connect-agent
+# Temporarily pin to version 1.34.4.2 as latest available version has a known issue.
+sudo yum install -y ecs-service-connect-agent-v1.34.4.2-*
