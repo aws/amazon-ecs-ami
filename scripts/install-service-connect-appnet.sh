60707517ae7da5697f92bcb60009209877a53d8d
Cameron Sparr <sparrc@users.noreply.github.com>
2026-03-12 14:49:49 -0700

    Revert "Temporarily pin ecs-service-connect-agent package to v1.34.4.2 (#629)"
    This reverts commit 505dc3d02cba46f6f41bd01596e8e1410b4bcc83.

--- scripts/install-service-connect-appnet.sh
+++ scripts/install-service-connect-appnet.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -ex
 
-# Temporarily pin to version 1.34.4.2 as latest available version has a known issue.
-sudo yum install -y ecs-service-connect-agent-v1.34.4.2-*
+sudo yum install -y ecs-service-connect-agent
